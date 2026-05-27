cf79b3b1738506945a86fd9544c6c98ef51d0ec4
Yash <yash4328gupta@gmail.com>
2026-05-27 18:16:36 +0530

    fix: export timeAgo utility to fix Vercel build

--- client/src/pages/Feed.jsx
+++ client/src/pages/Feed.jsx
@@ -5,22 +5,7 @@ import { api } from '../services/api';
 import { useAuth } from '../context/AuthContext';
 import { getStandardLocation, getSynonymsForLocation } from '../utils/locations';
 
-// Simple time formatter
-const timeAgo = (dateString) => {
-  const date = new Date(dateString);
-  const seconds = Math.floor((new Date() - date) / 1000);
-  let interval = seconds / 31536000;
-  if (interval > 1) return Math.floor(interval) + " years ago";
-  interval = seconds / 2592000;
-  if (interval > 1) return Math.floor(interval) + " months ago";
-  interval = seconds / 86400;
-  if (interval > 1) return Math.floor(interval) + " days ago";
-  interval = seconds / 3600;
-  if (interval > 1) return Math.floor(interval) + " hours ago";
-  interval = seconds / 60;
-  if (interval > 1) return Math.floor(interval) + " minutes ago";
-  return Math.floor(seconds) + " seconds ago";
-};
+import { timeAgo } from '../utils/timeAgo';
 
 // Keyword Highlighter component
 function HighlightText({ text, highlight }) {
